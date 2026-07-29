3330c67ba8aa0681a6b101462163f7747b7f5b05
DRAGOROD <jitmishra40@gmail.com>
2026-07-30 00:35:49 +0530

    bmi calculator almost done

--- gym-website/src/calculator.jsx
+++ gym-website/src/calculator.jsx
@@ -3,35 +3,105 @@ import { useState } from 'react'
 const calculator = () => {
 
 let [bmi,setBmi]=useState(null);
+let [heightUnit,setHeightUnit]=useState("cm");
 let [height,setHeight]=useState(0);
+let [weightUnit,setWeightUnit]=useState("kg");
 let [weight,setWeight]=useState(0);
 let [age,setAge]=useState(0);
 let [gender,setGender]=useState("male");
+let [activity,setActivity]=useState("1.2");
 let [calories,setCalories]=useState(0);
 
-function calculateBmi(){
-  setBmi(weight / ((height/100)**2))
+
+
+/*
+Men:
+Daily Calories = ((10 *weight in kg) + (6.25 *height in cm) - (5 *age in years) + 5)*activity point
+Women:
+Daily Calories = ((10 *weight in kg) + (6.25 *height in cm) - (5 * age in years) - 161)*activity point 
+*/
+
+function calculateCalories(){
+return setCalories(
+gender==="male"?((10*(weightUnit==="lbs"?weight/2.20462:weight))+(6.25*(heightUnit==="m"?height*100:height))+(5*age)+5)*activity:((10*(weightUnit==="lbs"?weight/2.20462:weight))+(6.25*(heightUnit==="m"?height*100:height))+(5*age)-161)*activity
+)
 }
+
+function calculateBmi(){
+ return setBmi((weightUnit=== "lbs" ? (weight/2.20462):weight) / ((heightUnit==="cm"? height/100:height)**2))
+
+}
+
+
 
 
   return (
     <div id="calculator-container">
+      <hr/>
+      <div id="calculator-box">
         <h1>BMI CALCULATOR</h1>
         <form>
-            <input type="number" placeholder='Height in cm.....' onChange={(e)=>setHeight(e.target.value)}/>
-            <input type="number" placeholder='Weight in kg.....' onChange={(e)=>setWeight(e.target.value)}/>
-            <input type="number" placeholder='Age.....' onChange={(e)=>setAge(e.target.value)}/>
-            <select onChange={(e)=>setGender(e.target.value)}>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
+           <div><input type="number" required placeholder='Height' onChange={(e)=>setHeight(Number(e.target.value))}/><select onChange={(e)=>setHeightUnit(e.target.value)}>
+                                                                                                        <option value="cm">CM</option>
+                                                                                                        <option value="m">M</option>
+                                                                                                     </select></div> 
+            <div><input type="number" required placeholder='Weight' onChange={(e)=>setWeight(Number(e.target.value))}/><select onChange={(e)=>setWeightUnit(e.target.value)} id="weight-unit">
+                                                                                                        <option value="kg">KG</option>
+                                                                                                        <option value="lbs">LBS</option>
+                                                                                                      </select></div> 
+            <input type="number" required placeholder='Age.....' onChange={(e)=>setAge(e.target.value)}/>
+            <label>Gender:
+               <select onChange={(e)=>setGender(e.target.value)}>
+                 <option value="male">Male</option>
+                 <option value="female">Female</option>
+               </select>
+            </label>
+            <label>Activity Level:
+            <select onChange={(e)=>setActivity(Number(e.target.value))}>
+              <option value="1.2">No Activity(No Exercise)</option>
+              <option value="1.375">Light Activity(1-3 Days/Week)</option>
+              <option value="1.55">Modarate Activity(3-5Days/Week)</option>
+              <option value="1.1.725">Very Activity(6-7 Days/Week)</option>
             </select>
-            <input type="number" placeholder='Calories..(Optional)...' onChange={(e)=>setCalories(e.target.value)}/>
-            <button type='button' onClick={calculateBmi}>Calculate BMI</button>
+            </label>
+            <button type='button' onClick={()=>{calculateBmi();calculateCalories();}}>Calculate BMI</button>
         </form>
-        <div id="display-bmi">
-        <div>{bmi}</div>
-        <div>{bmi>=40?"Morbid Obesity":bmi>=35 && bmi<=39.9?"Obesity Class II":bmi>=30 && bmi<=34.9?"Obesity Class I":bmi>=25 && bmi<=29.9?"Overweight":bmi>=18.4 && bmi<=24.9?"Healthy Weight":bmi>=16.9 && bmi<18.4?"Mild Thinness":bmi>=16?"Moderate Thinness":"Severe Thinness"}</div>
+        <div id="display-bmi" hidden={bmi === null}>
+        <div >{bmi?bmi.toFixed(2):0}<span>Kg/M2</span></div>
+        <div style={{color: bmi >= 40 ? "red" : bmi >= 35 ? "orange" : bmi >= 30 ? "yellow" : bmi >= 25 ? "lightblue" : bmi >= 18.4 ? "green" : bmi>0?"darkred":"white"}}>
+            ({bmi>=40?"Morbid Obesity":bmi>=35 && bmi<=39.9?"Obesity Class II":bmi>=30 && bmi<=34.9?"Obesity Class I":bmi>=25 && bmi<=29.9?"Overweight":bmi>=18.4 && bmi<=24.9?"Healthy Weight":bmi>=16.9 && bmi<18.4?"Mild Thinness":bmi>16?"Moderate Thinness":bmi>0?"Severe Thinness":"Enter Input"})
         </div>
+        <div>{calories.toFixed(0)}<span>Kcal/Day</span></div>
+        <div id="diet-cards">
+          <div id="weight-loss">
+            <div>
+              <span style={{color:'green'}}>Slow:{calories-300}</span>
+              <span style={{color:'blue'}}>Modarate:{calories-500}</span>
+              <span style={{color:'red'}}>Aggressive:{calories-800}</span>
+            </div>
+            <div>
+              <span style={{color:'orangered'}}>Protine:<span style={{color:'white'}}>{Math.round(weight*1.2)}g</span></span>
+              <span style={{color:'orangered'}}>Protine:<span style={{color:'white'}}>{Math.round(weight*1.2)}g</span></span>
+              <span style={{color:'orangered'}}>Protine:<span style={{color:'white'}}>{Math.round(weight*1.2)}g</span></span>
+            </div>
+            <div>
+              <span style={{color:'orangered'}}>Fat:<span style={{color:'white'}}>{Math.round(((calories-300)*0.25)/9)}g</span></span>
+              <span style={{color:'orangered'}}>Fat:<span style={{color:'white'}}>{Math.round(((calories-500)*0.25)/9)}g</span></span>
+              <span style={{color:'orangered'}}>Fat:<span style={{color:'white'}}>{Math.round(((calories-800)*0.25)/9)}g</span></span>
+            </div>
+            <div>
+              <span style={{color:'orangered'}}>Carb:<span style={{color:'white'}}>{Math.round(((calories-300)-((weight*1.2)*4)-(((calories-300)*0.25)/9)*9)/4)}g</span></span>
+              <span style={{color:'orangered'}}>Carb:<span style={{color:'white'}}>{Math.round(((calories-500)-((weight*1.2)*4)-(((calories-500)*0.25)/9)*9)/4)}g</span></span>
+              <span style={{color:'orangered'}}>Carb:<span style={{color:'white'}}>{Math.round(((calories-800)-((weight*1.2)*4)-(((calories-800)*0.25)/9)*9)/4)}g</span></span>
+            </div>
+          </div>
+          <div id="weight-gain">
+
+          </div>
+        </div>
+        </div>
+        </div>
+        <hr/>
     </div>
   )
 }
